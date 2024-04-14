17e7ab7c3a5aa965498383242aed253e3be47749
mitrus <antonreborn777@gmail.com>
2024-04-13 19:35:16 -0700

    Polished look.

--- utils/tmux-sessionizer.sh
+++ utils/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Projects ~/RustroverProjects ~/CLionProjects ~/.config ~ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/.dotfiles ~/Projects ~/RustroverProjects ~/CLionProjects ~/.config ~ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
