4e6c3dba6e1fbde8e5c649818eb02cb29e728b2d
mitrus <antonreborn777@gmail.com>
2024-04-13 17:52:34 -0700

    Adjusted to linux.

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,4 +1,9 @@
 function cat --wraps=bat --description 'alias cat=bat'
-  bat $argv
+  set -l os (uname)
+  if test "$os" = Darwin
+    bat $argv
+  else if test "$os" = Linux
+    batcat $argv
+  end
         
 end
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,9 @@
 function ls --wraps=exa --wraps='/bin/ls --hyperlink=auto --color=auto ' --wraps='gls --hyperlink=auto --color=auto' --description 'alias ls=gls --hyperlink=auto --color=auto'
-  gls --hyperlink=auto --color=auto $argv
+  set -l os (uname)
+  if test "$os" = Darwin
+    gls --hyperlink=auto --color=auto $argv
+  else if test "$os" = Linux
+    command ls --hyperlink=auto --color=auto $argv
+  end
         
 end
