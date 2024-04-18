81564f06c548dd3ed1d0735d6d7a646d178ec16f
mitrus <antonreborn777@gmail.com>
2024-04-18 14:08:41 -0700

    Fixed cursor in tmux.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,12 @@
+fish_vi_key_bindings
+
 if status is-interactive
     if string match -q -- 'tmux*' $TERM
         set -g fish_vi_force_cursor 1
+
+        set fish_cursor_default block
+        set fish_cursor_insert line
+        set fish_cursor_replace_one underscore
     end
 end
 
@@ -8,7 +14,7 @@ fzf_configure_bindings --directory=\cf
 set fzf_preview_dir_cmd exa --all --color=always
 set fzf_preview_file_cmd bat -n
 set fzf_fd_opts --hidden --max-depth 5
-
+    
 function init-cppcmake
     # Clone the GitHub repository
     git clone https://github.com/AntonReborn/CMakeTemplate
